8c8efa6fba1b89ede36176f51172e92a420a511f
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2020-10-21 19:48:20 +0530

    :five: Imp of nextLine() in String

--- 05_stdinstdout1.java
+++ 05_stdinstdout1.java
@@ -18,3 +18,8 @@ public class Solution {
         System.out.println("Int: " + i);
     }
 }
+
+/*After supplying data for int, we would hit the enter key. What nextInt() and nextDouble() does is it assigns integer to appropriate variable and 
+keeps the enter key unread in thekeyboard buffer. 
+so when its time to supply String the nextLine() will read the enter key from the user thinking that the user has entered the enter key.(that's we get empty output) . 
+Unlike C, there is no fflush() to clean buffer, so we have to flush by not taking it in variable. */
